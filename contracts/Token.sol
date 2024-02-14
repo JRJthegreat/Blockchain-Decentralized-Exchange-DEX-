3f7b6650329b92a9a6e7f504eee92c80d553d954
JOSEPH Rood Judeley <jroodjudeley@gmail.com>
2024-02-14 21:33:21 +0800

    4. Token balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,12 +10,16 @@ contract Token {
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
 
+	//track balances 
+	mapping (address => uint256) public balanceOf;
+	
+
 	constructor (string memory _name,string memory _symbol,uint256 _totalSupply){
 		name        = _name;
 		symbol      = _symbol;
-		totalSupply = _totalSupply *(10**decimals);//1000000*10^18;
+		totalSupply = _totalSupply * (10**18);
+		balanceOf[msg.sender]  = totalSupply;
 		
-
 	}
 		
 }
--- test/Token.js
+++ test/Token.js
@@ -7,37 +7,56 @@ const tokens = (n)=>{
 
 describe('Token',()=> {
 	//declare the variable that will contain the the delopyed version of the token first so both of te iterations will be able to read it 
-	let token
+	let token, accounts ,deployer
+	
 	//some javascript to set some variables upfrot if running multiple tests
 	beforeEach(async()=>{
 		//Fetch Token from blockchain
 		const Token = await ethers.getContractFactory('Token')
 		token = await Token.deploy('Haitian Gourde','HTG','1000000')
 
+		accounts = await ethers.getSigners()
+		deployer = accounts[0]
+
 	})
 
-	it('has correct name', async()=> {
+	describe('deployment',()=>{
+		const name 			= 'Haitian Gourde'
+		const symbol		= 'HTG'
+		const decimals		= '18'
+		const totalSupply 	= tokens('1000000')
+
+		it('has correct name', async()=> {
 		//check that the name is correct
-		expect(await token.name()).to.equal('Haitian Gourde')
+			expect(await token.name()).to.equal('Haitian Gourde')
 
-	})
+		})
 
-	it('has correct symbol', async()=> {
+		it('has correct symbol', async()=> {
 		 
 		//check that the symbol is correct
-		expect(await token.symbol()).to.equal('HTG')
-	})
+			expect(await token.symbol()).to.equal('HTG')
+		})
 
-	it('has correct decimals', async()=> {
+		it('has correct decimals', async()=> {
 		 
 		//check that the symbol is correct
-		expect(await token.decimals()).to.equal('18')
-	})
+			expect(await token.decimals()).to.equal('18')
+		})
 
-	it('has correct total supply', async()=> {
+		it('has correct total supply', async()=> {
 		
 		 
 		//check that the total supply is correct
-		expect(await token.totalSupply()).to.equal( tokens('1000000'))
+			expect(await token.totalSupply()).to.equal( tokens('1000000'))
+		})
+
+		it('attributes total supply to balance', async()=> {
+		 
+		//check that the symbol is correct
+			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+		})
 	})
+
+	
 })
